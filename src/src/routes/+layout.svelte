1c038f8eccab4a6f166c63bf303bb743262f6607
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2022-12-23 23:28:19 +0100

    added initial images and graphics

---
+++ src/src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Nav from "../components/nav.svelte";
+</script>
+  
+
+<Nav></Nav>
+
+<slot></slot>
+
+<style>
+    :global(body) {
+        background-color: #000000;
+        padding: 0;
+        margin: 0;
+    }
+
+    :global(h1) {
+        color: #ffffff;
+    }
+    :global(p) {
+        color: #ffffff;
+    }
+</style>
