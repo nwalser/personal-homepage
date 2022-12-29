0b8a7c52065ff386ef923967080056c11c3a5eec
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2022-12-29 21:04:20 +0100

    updated timeline look

--- src/src/components/timeline/TimelineHeading.svelte
+++ src/src/components/timeline/TimelineHeading.svelte
@@ -13,25 +13,25 @@
 		<div class="relative flex items-start space-x-3">
 			<div class="relative">
 				<img
-					class="flex h-12 w-12 items-center justify-center rounded-md bg-gray-200 ring-8 ring-white"
+					class="flex h-12 w-12 items-center justify-center rounded-md my-2 bg-gray-200 ring-8 ring-white"
 					src={src}
 					alt={alt}
 				/>
 			</div>
 			<div class="min-w-0 flex-1">
 				<div>
-					<div class="text-sm">
+					<div class="text-xl font-medium text-gray-900">
 						{#if href!=""}
 							{#if newTarget}
-								<a href={href} target="_blank" rel="noopener noreferrer" class="font-medium text-gray-900 text-base">{title}</a>
+								<a href={href} target="_blank" rel="noopener noreferrer">{title}</a>
 							{:else}
-								<a href={href} class="font-medium text-gray-900">{title}</a>
+								<a href={href}>{title}</a>
 							{/if}
 						{:else}
-							<p class="font-medium text-gray-900">{title}</p>
+							<p>{title}</p>
 						{/if}
 					</div>
-					<p class="mt-0.5 text-sm text-gray-500">{subtitle}</p>
+					<p class="mt-0.5 text-xl text-gray-500">{subtitle}</p>
 				</div>
 			</div>
 		</div>
