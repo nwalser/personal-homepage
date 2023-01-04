5ec4fe4616d041a9c6391d96d178b7996f8d5017
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2023-01-04 15:30:27 +0100

    added code blocks

--- src/src/components/code-block/CodeBlockTab.svelte
+++ src/src/components/code-block/CodeBlockTab.svelte
@@ -3,8 +3,12 @@
 	import { TABS } from './CodeBlock.svelte';
 
 	export let name: string;
+    export let icon: string = "";
 
-	let tab = {name: name};
+	let tab = {
+        name: name,
+        icon: icon
+    };
 	const { registerTab, selectedTab } = getContext(TABS);
 
 	registerTab(tab);
