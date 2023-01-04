8fdf4940e4863ca3bab7fecdf13291bca065de97
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2023-01-04 23:22:22 +0100

    updated docker compose template overview page

--- src/src/components/post-list/PostListItem.svelte
+++ src/src/components/post-list/PostListItem.svelte
@@ -8,18 +8,18 @@
 
 
 <div
-	class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
+	class="group relative flex flex-col overflow-hidden rounded-lg bg-white"
 >
-	<div class="bg-gray-200">
+	<div class="bg-gray-600">
 		<img
 			src={src}
 			alt={alt}
-			class="aspect-[8/3] object-cover object-center"
+			class="aspect-[8/3] object-fit object-center p-10"
 		/>
 	</div>
 	<div class="flex flex-1 flex-col space-y-2 p-4">
-		<h3 class="text-sm font-medium text-gray-900">
-			<a href={href}>
+		<h3 class="text-sm font-bold text-gray-900">
+			<a href={href} class="text-lg">
 				<span aria-hidden="true" class="absolute inset-0" />
 				{title}
 			</a>
--- src/src/routes/docker-compose-templates/+page.svelte
+++ src/src/routes/docker-compose-templates/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import { MetaTags } from 'svelte-meta-tags';
 	import Panel from 'src/components/Panel.svelte';
-	import TwoColumnLayout from 'src/layouts/TwoColumnLayout.svelte';
 	import Prose from 'src/components/Prose.svelte';
 	import Hero from 'src/components/headers/Hero.svelte';
+	import MainLayout from 'src/layouts/MainLayout.svelte';
+	import PostList from 'src/components/post-list/PostList.svelte';
+	import PostListItem from 'src/components/post-list/PostListItem.svelte';
 </script>
 
 <MetaTags
@@ -12,7 +14,7 @@
 	description="Templates for docker compose to be able to start quickly."
 />
 
-<TwoColumnLayout>
+<MainLayout>
 	<span slot="header">
 		<Hero src="/hero/bits.svg" alt="programmer working at desk in office" />
 	</span>
@@ -20,13 +22,17 @@
 		<Panel>
 			<Prose>
 				<h1>Docker Compose Templates</h1>
-				<a
-					href="/docker-compose-templates/eventstore"
-					class="inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-					>Docker Compose template for EventStoreDb</a
-				>
 			</Prose>
 		</Panel>
+
+		<PostList>
+			<PostListItem
+				title="Docker Compose template for EventStoreDb"
+				href="/docker-compose-templates/eventstore"
+				subtitle="This Docker Compose Template gets you started with EventStoreDb quickly."
+				src="/docker-compose-templates/eventstore/header.svg"
+				alt="eventstore db logo"
+			/>
+		</PostList>
 	</span>
-	<span slot="side" />
-</TwoColumnLayout>
+</MainLayout>
