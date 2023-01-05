3c7869af7bbcf148b5d4db68bbf88a6376e71686
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2023-01-05 14:10:22 +0100

    added minio page

--- src/src/components/post-list/PostListItem.svelte
+++ src/src/components/post-list/PostListItem.svelte
@@ -4,26 +4,27 @@
     export let src: string;
     export let alt: string;
     export let href: string;
+	export let color: string = "bg-gray-600";
 </script>
 
 
 <div
 	class="group relative flex flex-col overflow-hidden rounded-lg bg-white"
 >
-	<div class="bg-gray-600">
+	<div class="{color}">
 		<img
 			src={src}
 			alt={alt}
-			class="aspect-[8/3] object-fit object-center p-10"
+			class="aspect-[8/3] object-fit object-center"
 		/>
 	</div>
 	<div class="flex flex-1 flex-col space-y-2 p-4">
-		<h3 class="text-sm font-bold text-gray-900">
-			<a href={href} class="text-lg">
+		<h2 class="font-bold text-gray-900 text-2xl">
+			<a href={href}>
 				<span aria-hidden="true" class="absolute inset-0" />
 				{title}
 			</a>
-		</h3>
+		</h2>
 		<p class="text-sm text-gray-500">
 			{subtitle}
 		</p>
