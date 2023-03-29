26b5cdd0de6c1f1e31c3d1f0bb786931c583ed91
Nathaniel Walser <nathaniel.walser@esp-engineering.com>
2023-03-30 01:42:59 +0200

    extended prepromt

--- src/src/components/chat/Chat.svelte
+++ src/src/components/chat/Chat.svelte
@@ -7,7 +7,7 @@
 	let firstMessage = `Hi! I am Nathaniel's online buddy. I can 
         answer basic questions you have about him so he doesn't 
         have to. But be aware, I might hallucinate sometimes. :) 
-        What do you want to know?`;
+        What do you want to know about Nathaniel?`;
 
 	let messages = writable<ChatCompletionRequestMessage[]>([
 		{
@@ -22,7 +22,6 @@
 	const onKeyPress = (e) => {
 		if (e.charCode === 13) chat();
 	};
-
 
 	async function chat() {
 		$writing = true;
@@ -51,13 +50,17 @@
 			{#each $messages as message}
 				{#if message.role == 'assistant'}
 					<p class="relative my-4 max-w-[90%] mr-auto p-4 text-justify bg-slate-100 rounded-lg">
-						<span class="font-bold absolute -top-5 left-3 bg-slate-200 p-2 py-0 rounded-md">Buddy</span>
+						<span class="font-bold absolute -top-5 left-3 bg-slate-200 p-2 py-0 rounded-md"
+							>Buddy</span
+						>
 						{message.content}
 					</p>
 				{/if}
 				{#if message.role == 'user'}
 					<p class="relative my-4 ml-auto p-4 max-w-[90%] text-justify bg-slate-100 rounded-lg">
-						<span class="font-bold absolute -top-5 right-3 bg-slate-200 p-2 py-0 rounded-md">You</span>
+						<span class="font-bold absolute -top-5 right-3 bg-slate-200 p-2 py-0 rounded-md"
+							>You</span
+						>
 						{message.content}
 					</p>
 				{/if}
@@ -65,24 +68,50 @@
 
 			{#if $writing == true}
 				<p class="relative my-4 max-w-[90%] p-4 mr-auto text-justify bg-slate-100 rounded-lg">
-					<span class="font-bold absolute -top-5 left-3 bg-slate-200 p-2 py-0 rounded-md">Buddy</span>
+					<span class="font-bold absolute -top-5 left-3 bg-slate-200 p-2 py-0 rounded-md"
+						>Buddy</span
+					>
 					writing...
 				</p>
 			{/if}
 		</div>
 
 		<div class="not-prose justify-end flex">
-			<input
-				bind:value={newMessage}
-				on:keypress={onKeyPress}
-				class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-				placeholder="Your question..."
-			/>
-			<button
-				on:click={chat}
-				class="ml-2 inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 mr-2"
-				>➤ Ask</button
-			>
+			{#if $messages.length > 5}
+				<input
+					disabled
+					class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:bg-gray-50"
+					placeholder="Conversation ended"
+				/>
+				<div
+					class="ml-2 inline-flex w-24 justify-center items-center rounded-md border border-transparent bg-gray-100 px-4 py-2 text-sm font-medium text-gray-500 mr-2"
+				>
+					➤ Ask
+				</div>
+			{:else if $writing == false}
+				<input
+					bind:value={newMessage}
+					on:keypress={onKeyPress}
+					class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+					placeholder="Your question..."
+				/>
+				<button
+					on:click={chat}
+					class="ml-2 inline-flex w-24 justify-center items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 mr-2"
+					>➤ Ask</button
+				>
+			{:else}
+				<input
+					disabled
+					class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 disabled:bg-gray-50"
+					placeholder="Your question..."
+				/>
+				<div
+					class="ml-2 inline-flex w-24 justify-center items-center rounded-md border border-transparent bg-gray-100 px-4 py-2 text-sm font-medium text-gray-500 mr-2"
+				>
+					➤ Ask
+				</div>
+			{/if}
 		</div>
 	</Prose>
 </Panel>
